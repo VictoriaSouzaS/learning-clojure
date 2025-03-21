a9290934911376bf4e0159fd4dd6d2351c364415
Victoria  <96148132+VictoriaSouzaS@users.noreply.github.com>
2025-03-21 10:05:13 -0300

    Fix config

--- src/config/app_config.clj
+++ src/config/app_config.clj
@@ -1,22 +1,14 @@
 #_{:clj-kondo/ignore [:namespace-name-mismatch]}
 (ns config.app-config
   (:require [integrant.core :as ig]
-            [adapter.output.todo-repository :as repo]
-            [adapter.input.todo-api :as api]
-            [core.ports.todo-port :as port]
-            [core.service.todo-service :as service]
-            [ring.adapter.jetty :as jetty])) ;; Adaptador do Ring para o servidor Jetty
+            [ring.adapter.jetty :as jetty]))
 
 (def config
-  {:todo-repository {}
-   :todo-api {:handler (api/handler)
-              :port 8081}})
+  {:todo-api {:port 8081}})
 
-(defmethod ig/init-key :todo-repository [_ _]
-  (repo/new-todo-repository))
-
-(defmethod ig/init-key :todo-api [_ {:keys [handler port]}]
-  (jetty/run-jetty handler {:port port}))
+(defmethod ig/init-key :todo-api [_ {:keys [port]}]
+  (let [api (requiring-resolve 'adapter.input.todo-api/handler)]  ;; Carrega dinamicamente o handler evitando ref circular
+    (jetty/run-jetty api {:port port})))
 
 (defn init []
   (ig/init config))
